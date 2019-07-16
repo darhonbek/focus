4f01bee4fefd2a1814f6044a34828bb34abb5520
Darkhonbek <darhonbek@gmail.com>
2019-07-16 20:43:20 +0500

    Initial Commit

---
+++ Focus/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Focus
+//
+//  Created by Darkhonbek Mamataliev on 16/7/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Focus/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Focus
+//
+//  Created by Darkhonbek Mamataliev on 16/7/19.
+//  Copyright © 2019 Darkhonbek Mamataliev. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
