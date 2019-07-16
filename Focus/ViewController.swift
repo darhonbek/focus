7b4ac4a1e757699ac6828b25997209fca6e57380
Darkhonbek <darhonbek@gmail.com>
2019-07-16 21:35:00 +0500

    Initial commit. Add script to close app.

--- Focus/ViewController.swift
+++ Focus/ViewController.swift
@@ -10,16 +10,55 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private lazy var button: NSButton = {
+        let button = NSButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.action = #selector(closeButtonClicked)
+        button.title = "Close Apps"
 
-        // Do any additional setup after loading the view.
+        return button
+    }()
+
+    override func loadView() {
+        super.loadView()
+
+        view.addSubview(button)
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.widthAnchor.constraint(equalToConstant: 100.0),
+            button.heightAnchor.constraint(equalToConstant: 20.0)
+            ])
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    @objc func closeButtonClicked() {
+        closeApps()
+    }
+
+    func closeApps() {
+        var runningApplications = NSWorkspace.shared.runningApplications
+
+        for application in runningApplications {
+            if let name = application.localizedName {
+                print(name)
+                if name == "Activity Monitor" {
+//                    kill(application.processIdentifier, SIGKILL)
+                    if application.terminate() {
+                        print("closed")
+                    } else {
+                        print("not closed")
+                    }
+                }
+            }
         }
+
+//        if let scriptObject = NSAppleScript(source: quitAppsAppleScript) {
+//            if let output: NSAppleEventDescriptor = scriptObject.executeAndReturnError(
+//                &error) {
+//                print(output.stringValue)
+//            } else if (error != nil) {
+//                print("error: \(error)")
+//            }
+//        }
     }
 
 
